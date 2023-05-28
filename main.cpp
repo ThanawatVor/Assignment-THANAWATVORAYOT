efb5d997df72226456c6da59c9ccb16d237b9d1c
ThanawatVor <132637521+ThanawatVor@users.noreply.github.com>
2023-05-28 15:22:49 +0000

    I am done

--- main.cpp
+++ main.cpp
@@ -6,22 +6,33 @@ using namespace std;
 
 
 int main(int argc, char **argv){
- int i,n,*a;
-			n=argc-1;
- 		     a=new int[n];
-			for(i=0;i<n;i++) {
-			    a[i]=atoi(argv[i]);
-		       cout<< "a[" << i << "]=" <<a[i]<<endl;
+ int i,N,*a,n,*p;
+			N=argc-1;
+ 		     a=new int[N];
+			for(i=0;i<N;i++) {
+			    a[i]=atoi(argv[i+1]);
+		         cout<< "a[" << i << "]=" <<a[i]<<endl;
 		}
-
-    int*p=a;
-    int*pb;
-
-
+  int *pa = &a[0]; 
+  int *pb = &a[N-1];
+  cout<<"Reverse: ";
+  for(i=0; i<N/2; i++, pa++,pb--)
+    {
+      int temp=*pa;
+      *pa=*pb;
+      *pb=temp;
+    }
+    pa=&a[0];
+    for(i=0; i<N; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
+   pa-=n/2;
   
      // Using p within your program 
   for(i=0;i<n;i++) {
- 		   cout<<p[i]<<" "; //p is the location where p points to
+ 		   cout<<pa[i]<<" "; //p is the location where p points to
  //     p++;  
  }
 
